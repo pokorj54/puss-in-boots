0050e06e2432bb6ed01696481026f4e0cebb0d22
Jan Pokorný <pokorj54@fit.cvut.cz>
2022-09-05 09:08:54 +0200

    better message handling

--- main.py
+++ main.py
@@ -21,13 +21,13 @@ cat_words = ['cat', 'kitty', 'puss', 'kitties']
 @client.event 
 async def on_message(message):
     msg = str(message.content).lower()
-    for w in cat_words:
-        if w in  msg:
-            await message.add_reaction('🐈')
-            break
+    if msg == '':
+        return
+    await message.add_reaction('🐱')
     if "fact" in msg:
         await message.channel.send(facts.get_fact())
     if "pic" in msg:
         await message.channel.send(pics.get_pic())
 
+    await message.add_reaction('🐈')
 client.run(TOKEN)
