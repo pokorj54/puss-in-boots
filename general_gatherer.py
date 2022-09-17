27f713b713f0e406f71dd9bd50f6a41cb1f6ff8b
Jan Pokorný <pokorj54@fit.cvut.cz>
2022-09-17 17:35:50 +0200

    general pic gatherer

--- general_gatherer.py
+++ general_gatherer.py
@@ -2,20 +2,23 @@ import json
 import requests
 
 
-class GeneralPicGatherer:
+class GeneralGatherer:
     def __init__(self, link, indexes, emote, trigger_words):
         self.link = link
         self.indexes = indexes
         self.emote = emote
         self.trigger_words = trigger_words
 
-    def get_pic(self):
-        data = requests.get(self.link).json()
+    def get(self):
+        data = requests.get(self.link)
+        if self.indexes == None:
+            return data
+        data = data.json()
         for index in self.indexes:
             data = data[index]
         return data
 
 if __name__ == "__main__":
-    fox_gatherer = GeneralPicGatherer('https://randomfox.ca/floof/', ['image'], '🦊', ['fox'])
-    print(fox_gatherer.get_pic())
+    test_gatherer = GeneralGatherer('https://some-random-api.ml/img/raccoon', ['link'], '🦝', ['raccoon', 'racoon']) # Raccoon
+    print(test_gatherer.get())
 
