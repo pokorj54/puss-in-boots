89699ffdf50d42cab93621f0fcc20a3cafab95cf
Jan Pokorný <pokorj54@fit.cvut.cz>
2022-09-17 17:25:59 +0200

    yo mama and kafi

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import my_secret_token
 import facts
 from cat_pic_gatherer import CatPicGatherer
-from general_pic_gatherer import GeneralPicGatherer
+from general_gatherer import GeneralGatherer
 
 TOKEN = my_secret_token.get_token()
 
@@ -11,21 +11,23 @@ intents = discord.Intents.default()
 
 client = discord.Client(intents=intents)
 
-pic_gatherers = [
+gatherers = [
     # CatPicGatherer(), # Cat
-    # GeneralPicGatherer('https://randomfox.ca/floof/', ['image'], '🦊', ['fox']),
-    GeneralPicGatherer('https://some-random-api.ml/img/birb', ['link'], '🐦', ['bird', 'birb']),
-    GeneralPicGatherer('https://some-random-api.ml/img/dog', ['link'], '🐶', ['dog', 'pupp']),
-    GeneralPicGatherer('https://some-random-api.ml/img/cat', ['link'], '🐈', ['cat', 'kitt', 'puss']),
-    GeneralPicGatherer('https://some-random-api.ml/img/fox', ['link'], '🦊', ['fox']),
-    GeneralPicGatherer('https://some-random-api.ml/img/raccoon', ['link'], '🦝', ['raccoon', 'racoon']),
-    GeneralPicGatherer('https://some-random-api.ml/img/panda', ['link'], '🐼', ['panda']),
-    GeneralPicGatherer('https://some-random-api.ml/img/kangaroo', ['link'], '🦘', ['kangaroo']),
-    GeneralPicGatherer('https://some-random-api.ml/img/koala', ['link'], '🐨', ['koala']),
-    GeneralPicGatherer('https://some-random-api.ml/img/red_panda', ['link'], '🔴', ['red panda']),
-    GeneralPicGatherer('https://random-d.uk/api/v1/random?type=png', ['url'], '🦆', ['duck', 'quack']),
-    GeneralPicGatherer('https://shibe.online/api/shibes?count=1', [0], '🦮', ['cheems', 'shiba']),
-    GeneralPicGatherer('https://zoo-animal-api.herokuapp.com/animals/rand', ['image_link'], '🐵', ['animal', 'zoo'])
+    # GeneralGatherer('https://randomfox.ca/floof/', ['image'], '🦊', ['fox']),
+    GeneralGatherer('https://some-random-api.ml/img/birb', ['link'], '🐦', ['bird', 'birb']),
+    GeneralGatherer('https://some-random-api.ml/img/dog', ['link'], '🐶', ['dog', 'pupp']),
+    GeneralGatherer('https://some-random-api.ml/img/cat', ['link'], '🐈', ['cat', 'kitt', 'puss']),
+    GeneralGatherer('https://some-random-api.ml/img/fox', ['link'], '🦊', ['fox']),
+    GeneralGatherer('https://some-random-api.ml/img/raccoon', ['link'], '🦝', ['raccoon', 'racoon']),
+    GeneralGatherer('https://some-random-api.ml/img/panda', ['link'], '🐼', ['panda']),
+    GeneralGatherer('https://some-random-api.ml/img/kangaroo', ['link'], '🦘', ['kangaroo']),
+    GeneralGatherer('https://some-random-api.ml/img/koala', ['link'], '🐨', ['koala']),
+    GeneralGatherer('https://some-random-api.ml/img/red_panda', ['link'], '🔴', ['red panda']),
+    GeneralGatherer('https://random-d.uk/api/v1/random?type=png', ['url'], '🦆', ['duck', 'quack']),
+    GeneralGatherer('https://shibe.online/api/shibes?count=1', [0], '🦮', ['cheems', 'shiba']),
+    GeneralGatherer('https://zoo-animal-api.herokuapp.com/animals/rand', ['image_link'], '🐵', ['animal', 'zoo']),
+    GeneralGatherer('https://coffee.alexflipnote.dev/random.json', ['file'], '☕', ['coffee', 'kafi']),
+    GeneralGatherer('https://api.yomomma.info/', ['joke'], '👩', ['yo mama', 'yo mamma', 'your mom'])
 ]
 
 
@@ -45,10 +47,10 @@ async def on_message(message):
         await message.channel.send(facts.get_fact())
         await message.add_reaction('📝')
         used = True
-    for gatherer in pic_gatherers:
+    for gatherer in gatherers:
         for w in gatherer.trigger_words:
             if w in msg:
-                await message.channel.send(gatherer.get_pic())
+                await message.channel.send(gatherer.get())
                 await message.add_reaction(gatherer.emote)
                 used = True
                 break
@@ -56,6 +58,6 @@ async def on_message(message):
         await message.channel.send("You can look me up at https://github.com/pokorj54/puss-in-boots. Contribution is welcomed.")
         used = True
     if 'list' in msg or not used:
-        await message.channel.send("I can do: \n" + "".join([str(g.trigger_words) + " -> " + g.emote + "\n" for g in pic_gatherers]))
+        await message.channel.send("I can do: \n" + "".join([str(g.trigger_words) + " -> " + g.emote + "\n" for g in gatherers]))
 
 client.run(TOKEN)
